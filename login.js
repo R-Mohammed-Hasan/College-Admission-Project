3b2fbab5962c8dfa4848546e1600222d9b101705
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2021-11-16 17:36:46 +0530

    adding

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
 function show(){
-  let a = document.getElementsByName("password")[0].tagName;
-  console.log(a);
-
+  let a = document.getElementsByClassName("password").type="password";
+  console.log(a.type="");
+  let b= document.getElementById("mail");
+  console.log(b.value);
+  let c = document.getElementById("number");
+  console.log(c.value);
+  //document.get
 }
